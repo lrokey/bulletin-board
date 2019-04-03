c062df57b8cc72df0e3309f1d37c452ea27949b4
Laura <rokey.laura@gmail.com>
2019-04-03 18:36:58 -0500

    create note

---
+++ src/Note.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FaPencilAlt } from 'react-icons/fa'
+import { FaTrash } from 'react-icons/fa'
+
+class Note extends React.Component {
+	constructor(props) {
+		super(props)
+		this.edit = this.edit.bind(this)
+		this.remove = this.remove.bind(this)
+	}
+
+	edit() {
+		alert ('editing note')
+	}
+	remove() {
+		alert ('removing note')
+	}
+
+	render() {
+		return (
+			<div className="note">
+				<p>Learn React</p>
+				<span>
+					<button onClick={this.edit}id="edit"><FaPencilAlt /></button>
+					/<button onClick={this.remove}id="remove"><FaTrash /></button>
+				</span>
+			</div>
+		)
+	}
+}
+
+export default Note
