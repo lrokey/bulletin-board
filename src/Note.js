b9debba9c0c0ba0f907bc23140fcc77c0cca694c
Laura <rokey.laura@gmail.com>
2019-04-03 18:52:19 -0500

    add save button

--- src/Note.js
+++ src/Note.js
@@ -1,22 +1,47 @@
 import React from 'react'
 import { FaPencilAlt } from 'react-icons/fa'
 import { FaTrash } from 'react-icons/fa'
+import { FaSave } from 'react-icons/fa'
 
 class Note extends React.Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			editing: false
+		}
 		this.edit = this.edit.bind(this)
 		this.remove = this.remove.bind(this)
+		this.renderForm = this.renderForm.bind(this)
+		this.renderDisplay = this.renderDisplay.bind(this)
+		this.save = this.save.bind(this)
 	}
 
 	edit() {
-		alert ('editing note')
+		this.setState({	
+			editing: true
+		})
 	}
+
 	remove() {
 		alert ('removing note')
 	}
 
-	render() {
+	save() {
+		alert(this._newText.value)
+	}
+
+	renderForm() {
+		return (
+			<div className="note">
+				<form>
+					<textArea ref={input => this._newText = input}/>
+					<button onClick={this.save}><FaSave /></button>
+				</form>
+			</div>
+		)
+	}
+
+	renderDisplay() {
 		return (
 			<div className="note">
 				<p>Learn React</p>
@@ -26,6 +51,10 @@ class Note extends React.Component {
 				</span>
 			</div>
 		)
+	}
+
+	render() {
+		return this.state.editing ? this.renderForm() : this.renderDisplay()
 	}
 }
 
